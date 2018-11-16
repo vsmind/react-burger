88043971eb04371bfad5f46ba0e653c6066a9ccd
Vitaly Semenov <vitaly.semenov@sparebank1.no>
2018-11-16 09:31:16 +0100

    update for css classes module name

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -351,6 +351,7 @@ module.exports = {
               importLoaders: 1,
               sourceMap: shouldUseSourceMap,
               modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]'
             }),
             // Don't consider CSS imports dead code even if the
             // containing package claims to have no side effects.
